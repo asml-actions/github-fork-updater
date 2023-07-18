fc2efb7a4e74aa3136714b2fa03cd29a34c1dc41
Jakub Niemczyk <jakub.niemczyk@asml.com>
2023-07-18 11:02:45 +0200

    update code-scan.js

--- code-scan.js
+++ code-scan.js
@@ -20,9 +20,24 @@ fs.readFile("updateResult.txt", "utf8", (err, data) => {
       compareUrl: item[3],
     };
   });
-  console.log(allData);
+  createPR(allData[0].issueLink);
 });
-console.log(allData);
+
+const createPR = async (issueLink) => {
+  const [_, owner, repository, compare] = issueLink.match(
+    /https:\/\/github.com\/([^/]+)\/([^/]+)\/compare\/([^/]+)\.\.\./
+  );
+  const [baseBranch, headBranch] = compare.split("..");
+  const prResponse = await octokit.pulls.create({
+    owner,
+    repo: repository,
+    title: "Test Pull Request",
+    head: headBranch,
+    base: baseBranch,
+  });
+  const pullRequest = prResponse.data;
+  console.log("Pull Request created:", pullRequest.html_url);
+};
 // const owner = "asml-actions";
 // const repo = "github-fork-updater";
 // const issueNumber = 184;
