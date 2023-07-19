c108bb9ce0b4ea866f27acad6466dbc52b3b7c91
Jakub Niemczyk <jakub.niemczyk@asml.com>
2023-07-19 13:15:55 +0200

    update validate-parent.yml and code-scan.js

--- code-scan.js
+++ code-scan.js
@@ -1,35 +1,56 @@
-const { Octokit } = require('@octokit/rest');
+const { Octokit } = require("@octokit/rest");
 const token = process.argv[2];
-
+const originalOwner = process.argv[3]
 const octokit = new Octokit({
   auth: token,
 });
-const forkOwner = 'asml-actions-validation';
-const forkRepoName = process.argv[3];
+const forkOwner = "asml-actions-validation";
+const RepoName = process.argv[3];
 
-console.log(`Using this repository: ${forkOwner}/${forkRepoName}`);
+console.log(`Using this repository: ${forkOwner}/${RepoName}`);
 
 async function deleteRepository() {
   try {
     const repository = await octokit.repos.get({
       owner: forkOwner,
-      repo: forkRepoName,
+      repo: RepoName,
     });
 
-    console.log(`Repository ${forkOwner}/${forkRepoName} already exists. Deleting before proceeding...`);
+    console.log(
+      `Repository ${forkOwner}/${RepoName} already exists. Deleting before proceeding...`
+    );
 
     await octokit.repos.delete({
       owner: forkOwner,
-      repo: forkRepoName,
+      repo: RepoName,
     });
 
-    console.log(`Successfully deleted repository ${forkOwner}/${forkRepoName}`);
+    console.log(`Successfully deleted repository ${forkOwner}/${RepoName}`);
   } catch (error) {
     if (error.status === 404) {
-      console.log(`Repository ${forkOwner}/${forkRepoName} is not found`);
+      console.log(`Repository ${forkOwner}/${RepoName} is not found`);
     } else {
-      console.log(`Deletion of repository ${forkOwner}/${forkRepoName} failed: ${error.message}`);
+      console.log(
+        `Deletion of repository ${forkOwner}/${RepoName} failed: ${error.message}`
+      );
     }
   }
 }
 deleteRepository();
+
+async function createFork() {
+  try {
+    const response = await octokit.repos.createFork({
+      owner: originalOwner,
+      repo: RepoName,
+      organization: forkOwner,
+    });
+
+    const forkedRepo = response.data;
+    console.log(`Fork created successfully: ${forkedRepo.html_url}`);
+  } catch (error) {
+    console.log(`Failed to create fork: ${error.message}`);
+  }
+}
+
+createFork();
