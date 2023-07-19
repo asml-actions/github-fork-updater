297a7ddf36b081c4ad4248eda4a9db4f19c6824f
Jakub Niemczyk <jakub.niemczyk@asml.com>
2023-07-19 14:53:41 +0200

    Update for async operation

--- code-scan.js
+++ code-scan.js
@@ -1,23 +1,21 @@
 const { Octokit } = require("@octokit/rest");
 const token = process.argv[2];
 const repoName = process.argv[3];
-const originalOwner = process.argv[4]
+const originalOwner = process.argv[4];
+const forkOwner = "asml-actions-validation";
+
 const octokit = new Octokit({
   auth: token,
 });
-const forkOwner = "asml-actions-validation";
-
-
 
 async function deleteRepository() {
-  console.log(`Deleting this repository: ${forkOwner}/${repoName}`);
-
   try {
     const repository = await octokit.repos.get({
       owner: forkOwner,
       repo: repoName,
     });
 
+    console.log(`Deleting this repository: ${forkOwner}/${repoName}`);
     console.log(
       `Repository ${forkOwner}/${repoName} already exists. Deleting before proceeding...`
     );
@@ -39,9 +37,8 @@ async function deleteRepository() {
   }
 }
 
-
 async function createFork() {
-  console.log(`forking ${originalOwner}/${repoName} to ${forkOwner}`)
+  console.log(`Forking ${originalOwner}/${repoName} to ${forkOwner}`);
   try {
     const response = await octokit.repos.createFork({
       owner: originalOwner,
@@ -56,22 +53,26 @@ async function createFork() {
   }
 }
 
-
 async function enableDependabot() {
   try {
-    const response = await octokit.request('PATCH /repos/{owner}/{repo}', {
+    console.log(`Enabling Dependabot for ${forkOwner}/${repoName}`);
+    const response = await octokit.request("PATCH /repos/{owner}/{repo}", {
       owner: forkOwner,
       repo: repoName,
       dependency_graph_enabled: true,
-      topics: ['dependabot', 'security'],
+      topics: ["dependabot", "security"],
     });
 
-    console.log('Dependabot enabled successfully.');
+    console.log("Dependabot enabled successfully.");
   } catch (error) {
     console.log(`Failed to enable Dependabot: ${error.message}`);
   }
 }
 
-deleteRepository();
-createFork();
-enableDependabot();
+async function run() {
+  await deleteRepository();
+  await createFork();
+  await enableDependabot();
+}
+
+run();
