171230cd31854f989b86ad839ce354c8203ff102
Jakub Niemczyk <jakub.niemczyk@asml.com>
2023-07-20 16:46:15 +0200

    update code-scan.js

--- code-scan.js
+++ code-scan.js
@@ -29,6 +29,7 @@ async function octokitRequest(request) {
     }
     const response = await octokitFunctions[request](requestProperties);
     console.log(`Function ${request} finished succesfully`);
+    console.log(response)
     return response;
   } catch (error) {
     console.log(`Failed to run ${request}: ${error.message}`);
