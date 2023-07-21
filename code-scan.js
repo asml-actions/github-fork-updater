310090402bd570c33c6771ebdd96887c7c9fade4
Jakub Niemczyk <jakub.niemczyk@asml.com>
2023-07-21 16:53:59 +0200

    update

--- code-scan.js
+++ code-scan.js
@@ -1,12 +1,12 @@
 const { Octokit } = require("@octokit/rest");
 const core = require("@actions/core");
 const { countReset } = require("console");
-const fs = require('fs');
+const fs = require("fs");
 const token = process.argv[2];
 const repo = process.argv[3];
 const originalOwner = process.argv[4];
 const owner = "asml-actions-validation";
-console.log (`repo name is ${repo}, original owner ${originalOwner}`)
+console.log(`repo name is ${repo}, original owner ${originalOwner}`);
 const octokit = new Octokit({
   auth: token,
 });
@@ -48,15 +48,19 @@ async function octokitRequest(request) {
   }
 }
 
-async function putRequest(request) { //generic function for PUT requests
+async function putRequest(request) {
+  //generic function for PUT requests
   try {
-    await octokit.request(`PUT /repos/{owner}/{repo}/${request}`, { owner, repo });
+    await octokit.request(`PUT /repos/{owner}/{repo}/${request}`, {
+      owner,
+      repo,
+    });
   } catch (error) {
     console.log(`Failed to run ${request}: ${error.message}`);
   }
 }
 
-async function getSha(ref){
+async function getSha(ref) {
   response = await octokit.rest.git.getRef({
     owner,
     repo,
@@ -65,30 +69,33 @@ async function getSha(ref){
   return response.data;
 }
 
-async function deleteExistingWorkflows(sha){
-  console.log(`Delete existing workflows`)
-  await octokit.rest.repos.deleteFile({
+async function disableExistingWorkflows() {
+  let workflow_ids = [];
+  const workflowList = await octokit.rest.actions.listRepoWorkflows({
     owner,
     repo,
-    path: ".github/workflows/codeql-analysis.yml",
-    message: "🤖 Delete existing workflows",
-    sha,
   });
-
+  workflow_ids = workflowList.data.workflows.map((item) => item.id);
+  workflow_ids.forEach((workflow_id) => {
+    octokit.rest.actions.disableWorkflow({
+      owner,
+      repo,
+      workflow_id,
+    });
+  });
 }
 
-async function pushWorkflowFile() { 
-
+async function pushWorkflowFile() {
   let languages = await octokitRequest("listLanguages");
   languages = `${JSON.stringify(Object.keys(languages))}`;
   console.log(`Detected languages: ${languages}`);
 
   console.log(`Add Codeql workflow file`);
   let workflowFile = fs.readFileSync("codeql-analysis-check.yml", "utf8");
-  workflowFile = workflowFile.replace('languageString',languages)
+  workflowFile = workflowFile.replace("languageString", languages);
 
-  console.log(`Add Codeql workflow file`)
-  
+  console.log(`Add Codeql workflow file`);
+
   try {
     const response = await octokit.request(
       "PUT /repos/{owner}/{repo}/contents/.github/workflows/codeql-analysis-check.yml",
@@ -107,8 +114,8 @@ async function pushWorkflowFile() {
   }
 }
 
-async function triggerCodeqlScan(workflow_id,ref){
-  console.log(`Trigger codeql scan`)
+async function triggerCodeqlScan(workflow_id, ref) {
+  console.log(`Trigger codeql scan`);
   await octokit.rest.actions.createWorkflowDispatch({
     owner,
     repo,
@@ -117,95 +124,93 @@ async function triggerCodeqlScan(workflow_id,ref){
   });
 }
 
-async function updateIssueLabel(label){
-  console.log(`Update the issue`)
+async function updateIssueLabel(label) {
+  console.log(`Update the issue`);
   await octokit.rest.issues.addLabels({
-    owner: 'asml-actions',
-    repo: 'github-fork-updater',
-    issue_number: '199',
-    labels: [ `${label}` ]
+    owner: "asml-actions",
+    repo: "github-fork-updater",
+    issue_number: "199",
+    labels: [`${label}`],
   });
 }
 
-async function waitForCodeqlScan(){
-  console.log(`Get the dispatched run id`)
+async function waitForCodeqlScan() {
+  console.log(`Get the dispatched run id`);
   const response = await octokit.rest.actions.listWorkflowRunsForRepo({
     owner,
     repo,
-    event: 'workflow_dispatch'
+    event: "workflow_dispatch",
   });
 
   const run_id = response.data.workflow_runs[0].id;
-  
-  let status = "queued"
-  while ( status != "completed")
-  {
-    console.log(`Wait for scan to complete - Run id : ${run_id}`)
-    await wait(15000)
+
+  let status = "queued";
+  while (status != "completed") {
+    console.log(`Wait for scan to complete - Run id : ${run_id}`);
+    await wait(15000);
     const run_status = await octokit.rest.actions.getWorkflowRun({
       owner,
       repo,
       run_id,
     });
-    if( run_status.data.status == "completed") {
-      status = run_status.data.status
+    if (run_status.data.status == "completed") {
+      status = run_status.data.status;
     }
   }
-
 }
 
-function checkForBlockingAlerts(codeScanningAlerts,dependabotAlerts){
-  let blocking = false
-  codeScanningAlerts.forEach(alert => {   
-    if(alert.rule.security_severity_level == "critical" || alert.rule.security_severity_level == "high"){
-      blocking = true
+function checkForBlockingAlerts(codeScanningAlerts, dependabotAlerts) {
+  let blocking = false;
+  codeScanningAlerts.forEach((alert) => {
+    if (
+      alert.rule.security_severity_level == "critical" ||
+      alert.rule.security_severity_level == "high"
+    ) {
+      blocking = true;
     }
   });
-  dependabotAlerts.forEach(alert => {
-    if(alert.security_advisory.severity =="critical" || alert.security_advisory.severity =="high" ){
-      blocking = true
+  dependabotAlerts.forEach((alert) => {
+    if (
+      alert.security_advisory.severity == "critical" ||
+      alert.security_advisory.severity == "high"
+    ) {
+      blocking = true;
     }
   });
 
-  return blocking
+  return blocking;
 }
 
 async function run() {
   await octokitRequest("delRepo");
   const forkRepo = await octokitRequest("createFork");
-  
-  await wait(5000);
-  await putRequest('vulnerability-alerts') // Enable dependabot
 
-  //Delete existing workflow files
-  const sha = (await getSha(forkRepo.default_branch)).object.sha
-    /* Fix file delete */
-  // deleteExistingWorkflows(sha)
+  await wait(5000);
+  await putRequest("vulnerability-alerts"); // Enable dependabot
+
+  disableExistingWorkflows()
   await wait(5000);
 
   // Push Codeql.yml file
-  await pushWorkflowFile()
+  await pushWorkflowFile();
 
-  
   //Trigger a scan
   await wait(15000);
-  await triggerCodeqlScan(`codeql-analysis-check.yml`,forkRepo.default_branch)
-  
+  await triggerCodeqlScan(`codeql-analysis-check.yml`, forkRepo.default_branch);
 
   //Wait for the scan to complete
-  console.log(`Wait for job to start !`)
-  await wait(15000)
-  await waitForCodeqlScan()
+  console.log(`Wait for job to start !`);
+  await wait(15000);
+  await waitForCodeqlScan();
 
   const dependabotAlerts = await octokitRequest("listAlertsForRepo");
   const codeqlScanAlerts = await octokitRequest("listScanningResult");
-  
-  if(checkForBlockingAlerts(codeqlScanAlerts,dependabotAlerts)){
-    core.setOutput('can-merge', 'needs-manual-check')
+
+  if (checkForBlockingAlerts(codeqlScanAlerts, dependabotAlerts)) {
+    core.setOutput("can-merge", "needs-manual-check");
   } else {
-    core.setOutput('can-merge', 'update-fork')
+    core.setOutput("can-merge", "update-fork");
   }
-  
 }
 
 run();
