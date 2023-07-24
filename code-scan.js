60432cb06ae86d069791e6a80f963fe9672abecc
Jakub Niemczyk <jakub.niemczyk@asml.com>
2023-07-24 16:43:08 +0200

    Add-dependabot-codeQL-container-scans: Fixed

--- code-scan.js
+++ code-scan.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const token = process.argv[2];
 const repo = process.argv[3];
 const originalOwner = process.argv[4];
-const owner = "asml-actions-validation";
+const owner = process.argv[5]
 console.log(`repo name is ${repo}, original owner ${originalOwner}`);
 const octokit = new Octokit({
   auth: token,
